43abbfa6e488fb622306faf5966b97d91fc59706
marder <marcin.derucki@gmail.com>
2025-03-14 19:35:04 +0100

    first commit

---
+++ 02_variables/02_variables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var number1 int = 10
+	fmt.Println(number1)
+
+	var number2 = 20
+	fmt.Println(number2)
+
+	number3 := 30
+	fmt.Println(number3)
+}
