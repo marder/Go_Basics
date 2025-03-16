8bd9ae9ccaa31498595ca5d1354dc4edfec94e27
marder <marcin.derucki@gmail.com>
2025-03-16 09:40:53 +0100

    16.03.2025

---
+++ 03_print/03_print.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	currentAge := 21
+	fmt.Printf("Age = %d\n", currentAge)
+	annualSalary := 65000.5
+	fmt.Printf("Annual Salary: %g", annualSalary)
+}
