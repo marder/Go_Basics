2e150045e54f7d3b5eeeb941118a4c9f4f8be7e6
marder <marcin.derucki@gmail.com>
2025-03-17 20:29:22 +0100

    17.03.2025

---
+++ 07_if_else/07_if_else.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	number := 13
+
+	if number > 0 {
+		fmt.Printf("%d is a positive number\n", number)
+	} else {
+		fmt.Printf("%d is a negative number\n", number)
+	}
+}
